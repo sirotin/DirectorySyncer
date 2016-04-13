8f30f92c50bcf5f6195e8b9351042a647727b1e0
Alexander Sirotin <sirotin@gmail.com>
2016-04-13 22:34:20 +0300

    Changed default log level to INFO

--- sync.py
+++ sync.py
@@ -177,7 +177,7 @@ class DirectorySyncer:
 
 def configure():
 	# Configure the logger
-	logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", datefmt="%d/%m/%Y %H:%M:%S", level=logging.DEBUG)
+	logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s", datefmt="%d/%m/%Y %H:%M:%S", level=logging.INFO)
 
 	# Read the command line arguments
 	parser = argparse.ArgumentParser()
